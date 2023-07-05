f59d0dc9f893a060daf2fd33ea787ccbd4e810ab
CodePeacock <mayursinalkar404@gmail.com>
2023-07-05 17:32:38 +0530

    added pdfs 😉

---
+++ WRITE_THE_FOLLOWING_PROGRAMS_FOR_BLOCKCHAIN_IN_PYTHON/A simple client class that generates the private and public keys by using the built-in Python RSA algorithm and test it.py
@@ -0,0 +1,23 @@
+import binascii
+
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+
+class Client:
+    def __init__(self):
+        random = Random.new().read
+        self._private_key = RSA.generate(1024, random)
+        self._public_key = self._private_key.public_key()
+        self._signer = PKCS1_v1_5.new(self._private_key)
+
+    @property
+    def identity(self):
+        return binascii.hexlify(self._public_key.export_key(format="DER")).decode(
+            "ascii"
+        )
+
+
+Dinesh = Client()
+print(f"\n Public Key: {Dinesh.identity}")
---
+++ prac1.py
@@ -0,0 +1,10 @@
+num1= int(input("Enter a number: "))
+num2= int(input("Enter another number: "))
+
+add = num1+num2
+sub = num1 - num2
+mult = num1 * num2
+
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mult}")
